b56e7794cdfc5b3aaf369fb0769fa5198fb7ceca
PineappleLord2020 <mul20002@byui.edu>
2024-01-23 15:19:17 -0700

    Added Classes to files

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        
+        var jobs = new List<Job>();
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Google";
+        job1._startYear = 2003;
+        job1._endYear = 2023;
+
+
+        Job job2 = new Job();
+        job2._jobTitle = "Graphic Designer";
+        job2._company = "Apple";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResumeDetails();
+        myResume.Display();
+
+
     }
 }
